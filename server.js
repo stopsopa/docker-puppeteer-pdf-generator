04d9de5dd9b2ae03c5ce7d043c5094db1f5b9eab
Szymon Dzialowski <sd@phase-ii.com>
2018-05-24 16:33:28 +0100

    update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
  * node server.js --port 7777
  */
 
-// const ll = require('../../react/webpack/logn');
+const ll = require('./lib/logn');
 
 const shelljs   = require('shelljs');
 
@@ -34,6 +34,26 @@ const config    = require('./config');
     }
 }());
 
+const parserParams = query => {
+    if (typeof query === 'string') {
+
+        query = query.split('&').reduce((acc, val) => {
+            var
+                a       = val.split(/=/),
+                key     = a.shift(),
+                dec     = a.join('=')
+            ;
+            acc[key] = decodeURIComponent(dec);
+            return acc;
+        }, {});
+    }
+    else {
+        query = {};
+    }
+
+    return query;
+};
+
 const auth      = require('basic-auth');
 
 var log = (function(){try{return console.log}catch(e){return function(){}}}());
@@ -44,6 +64,8 @@ const sync = require('child_process').spawnSync;
 const fs = require('fs');
 
 const path = require('path');
+
+var getRawBody = require('raw-body')
 
 var tlog = (function () {
     try {
@@ -163,199 +185,313 @@ server.listen(port, e => {
     tlog(`\n\n    server is listening on port ${port}\n\n`)
 });
 
-const html =  fs.readFileSync('./form.html').toString();
+const html =  fs.readFileSync('./form.html').toString().replace('<script type="json"></script>', '<script type="json">'+"\n"+JSON.stringify(config.defaults, null, 4)+"\n"+'</script>');
 
 let purl = false;
 
+var type = (function (types) {
+    return function (req, res, ext) {
+
+        ext = ext || path.extname(req.url.toLowerCase().split('?')[0]).replace(/[^a-z0-9]/g, '');
+
+        types[ext] && res.setHeader('Content-Type', types[ext]);
+
+        return ext;
+    }
+}((function (type) {
+    type.jpeg = type.jpg;
+    return type;
+}({
+    html    : 'text/html; charset=utf-8',
+    js      : 'application/javascript; charset=utf-8',
+    css     : 'text/css; charset=utf-8',
+    json    : 'application/json; charset=utf-8',
+    txt     : 'text/plain; charset=utf-8',
+    gif     : 'image/gif',
+    bmp     : 'image/bmp',
+    jpg     : 'image/jpeg',
+    png     : 'image/png',
+    pdf     : 'application/pdf',
+    ico     : 'image/x-icon',
+}))));
+
 const handler = (req, res, next) => {
 
-    const pathname = (function () {
-        return req.url.split('?')[0];
-    }());
+    getRawBody(req)
+    .then(function (buf) {
 
-    // http://localhost:7777/pdf-generator-check
-    if (pathname === '/infinity') {
+        let parsed;
 
-        return next();
-    }
+        try {
 
-    // http://localhost:7777/pdf-generator-check
-    if (pathname === '/pdf-generator-check') {
+            // expecting here: {
+            //     "url": "",
+            //     "launch": {
+            //     },
+            //     "pdf": {
+            //         "printBackground": true,
+            //             "format": "A4",
+            //             "margin": {
+            //             "top": 0,
+            //                 "right": 0,
+            //                 "bottom": 0,
+            //                 "left": 0
+            //         },
+            //         "scale": 0.7
+            //     }
+            // }
+            parsed = JSON.parse(buf.toString());
+        }
+        catch (e) {
 
-        res.end('ok');
+            // here I'm expecting:
+            // "url=https%3A%2F%2Fstackoverflow.com%2Fquestions%2F22907231%2Fcopying-files-from-host-to-docker-container&json=%7B%22launch%22%3A%7B%7D%2C%22pdf%22%3A%7B%22printBackground%22%3Atrue%2C%22format....
+            // post form field "url" and separately "json"
+            // but at the end I'm combining this to format that is visible in try higher
 
-        return next();
-    }
+            parsed = parserParams(buf.toString());
 
-    if (pathname === '/favicon.ico') {
+            if (parsed.json && typeof parsed.json == 'string') {
 
-        res.end('no favicon');
+                try {
 
-        return next();
-    }
+                    parsed.json = JSON.parse(parsed.json);
 
-    var credentials = auth(req);
+                    parsed = {
+                        url: parsed.url,
+                        ...parsed.json
+                    }
+                }
+                catch (e) {
 
-    if (!credentials || credentials.name !== config.basicAuth.name || credentials.pass !== config.basicAuth.password) {
-
-        res.statusCode = 401;
-        res.setHeader('WWW-Authenticate', 'Basic realm="User and password please"')
-        res.end('Access denied');
-
-        return next();
-    }
-
-    const jsonResponse = json => {
-
-        if (json.error) {
-
-            res.statusCode = 500;
+                }
+            }
         }
 
-        res.setHeader('Content-type', 'application/json; charset=utf-8');
+        // {
+        //     "url": "",
+        //     "json": {
+        //     "launch": {},
+        //     "pdf": {
+        //         "printBackground": true,
+        //             "format": "A4",
+        //             "margin": {
+        //             "top": 0,
+        //                 "right": 0,
+        //                 "bottom": 0,
+        //                 "left": 0
+        //         },
+        //         "scale": 0.7
+        //     }
+        // }
 
-        res.end(JSON.stringify(json, null, 4));
+        const pathname = (function () {
+            return req.url.split('?')[0];
+        }());
 
-        return next();
-    }
+        if (/^\/js\//.test(pathname)) {
 
-    log(`\n    vv new request vv ` + req.url);
+            var file = path.resolve(__dirname, '.' + path.sep + (decodeURI(pathname).replace(/\.\.+/g, '.')));
 
-    if (pathname !== '/generate') {
+            if (fs.existsSync(file)) {
 
-        res.setHeader('Content-type', 'text/html; charset=UTF-8');
+                try {
 
-        res.end(`The only endpoint handled is <a href="/generate">/generate</a>`);
+                    next();
 
-        return next();
-    }
-    // purl = false;
+                    return res.end(fs.readFileSync(file), type(req, res));
+                }
+                catch (e) {
 
-    let sel;
+                    next();
 
-    try {
-
-        if ( purl ) {
-
-            tlog(req.url + ' : server busy');
-
-            purl = false;
-
-            return jsonResponse({
-                error: `server is currently processing: ${purl}`
-            });
+                    return res.end('No access to file... ' + file);
+                }
+            }
         }
 
-        purl = true;
-
-        let query = req.url.split('?')[1];
-
-        if (typeof query === 'string') {
-
-            query = query.split('&').reduce((acc, val) => {
-                var
-                    a       = val.split(/=/),
-                    key     = a.shift(),
-                    dec     = a.join('=')
-                ;
-                acc[key] = decodeURIComponent(dec);
-                return acc;
-            }, {});
-        }
-        else {
-            query = {};
-        }
-
-        purl = query.url;
-
-        if ( ! purl ) {
-
-            purl = false;
-
-            res.end(html);
+        // http://localhost:7777/pdf-generator-check
+        if (pathname === '/infinity') {
 
             return next();
         }
 
-        tlog(`generating pdf from page: ${purl}`);
+        // http://localhost:7777/pdf-generator-check
+        if (pathname === '/pdf-generator-check') {
 
-        if ( ! /^https?:\/\/.+/.test(purl) ) {
+            res.end('ok');
 
-            tlog(purl + ' : url is not valid');
-
-            purl = false;
-
-            return jsonResponse({
-                error: `given url parameter (${query.url}) is not valid URL`
-            });
+            return next();
         }
 
-        const cmd = `/bin/bash pdf.sh "${purl}"`;
+        if (pathname === '/favicon.ico') {
+
+            res.end('no favicon');
+
+            return next();
+        }
+
+        var credentials = auth(req);
+
+        if (!credentials || credentials.name !== config.basicAuth.name || credentials.pass !== config.basicAuth.password) {
+
+            res.statusCode = 401;
+            res.setHeader('WWW-Authenticate', 'Basic realm="User and password please"')
+            res.end('Access denied');
+
+            return next();
+        }
+
+        const jsonResponse = json => {
+
+            if (json.error) {
+
+                res.statusCode = 500;
+            }
+
+            res.setHeader('Content-type', 'application/json; charset=utf-8');
+
+            res.end(JSON.stringify(json, null, 4));
+
+            return next();
+        }
+
+        log(`\n    vv new request vv ` + req.url);
+
+        if (pathname !== '/generate') {
+
+            res.setHeader('Content-type', 'text/html; charset=UTF-8');
+
+            res.end(`The only endpoint handled is <a href="/generate">/generate</a>`);
+
+            return next();
+        }
+        // purl = false;
+
+        let sel;
 
         try {
 
-            sel = shelljs.exec(cmd);
+            if ( purl ) {
+
+                tlog(req.url + ' : server busy');
+
+                return jsonResponse({
+                    error: `server is currently processing: ${purl}`
+                });
+            }
+
+            purl = true;
+
+            let query = parserParams(req.url.split('?')[1]);
+
+            purl = (function () {
+                try {
+                    return parsed.url;
+                }
+                catch (e) {}
+            }());
+
+            if (query.url) {
+
+                purl = query.url;
+            }
+
+            if ( ! purl ) {
+
+                purl = false;
+
+                res.end(html);
+
+                return next();
+            }
+
+            tlog(`generating pdf from page: ${purl}`);
+
+            if ( ! /^https?:\/\/.+/.test(purl) ) {
+
+                tlog(purl + ' : url is not valid');
+
+                purl = false;
+
+                return jsonResponse({
+                    error: `given url parameter (${query.url}) is not valid URL`
+                });
+            }
+
+            const cmd = `/bin/bash pdf.sh "${purl}"`;
+
+            try {
+
+                sel = shelljs.exec(cmd);
+            }
+            catch (e) {
+
+                tlog('exception:');
+
+                purl = false;
+
+                return jsonResponse({
+                    error: 'innerCatch',
+                    exception: e.toString()
+                });
+            }
+
+            const file = path.join(__dirname, 'pdf.pdf');
+
+            if (sel.code != 0) {
+
+                tlog(`executing command '${cmd}' failed, exit code: '${sel.code}'`)
+
+                purl = false;
+
+                return jsonResponse({
+                    error: `processing url '${query.url}' failed`
+                });
+            }
+
+            if (fs.existsSync(file)) {
+
+                tlog('attempt to return file: ' + file + ' from server');
+
+                const stream = fs.createReadStream(file);
+                const stat = fs.statSync(file);
+                res.setHeader('Content-Length', stat.size);
+                res.setHeader('Content-Type', 'application/pdf');
+
+                // force browser to download
+                // res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
+
+                stream.pipe(res);
+
+                purl = false;
+
+                return setTimeout(next, 0);
+            }
+            else {
+
+                jsonResponse({
+                    file: path.join(__dirname, 'pdf.pdf'),
+                    error: 'file was not created'
+                })
+            }
         }
         catch (e) {
 
-            tlog('exception:');
-
-            purl = false;
-
-            return jsonResponse({
-                error: 'innerCatch',
-                exception: e.toString()
-            });
-        }
-
-        const file = path.join(__dirname, 'pdf.pdf');
-
-        if (sel.code != 0) {
-
-            tlog(`executing command '${cmd}' failed, exit code: '${sel.code}'`)
-
-            purl = false;
-
-            return jsonResponse({
-                error: `processing url '${query.url}' failed`
-            });
-        }
-
-        if (fs.existsSync(file)) {
-
-            tlog('attempt to return file: ' + file + ' from server');
-
-            const stream = fs.createReadStream(file);
-            const stat = fs.statSync(file);
-            res.setHeader('Content-Length', stat.size);
-            res.setHeader('Content-Type', 'application/pdf');
-
-            // force browser to download
-            // res.setHeader('Content-Disposition', 'attachment; filename=quote.pdf');
-
-            stream.pipe(res);
-
-            purl = false;
-
-            return setTimeout(next, 0);
-        }
-        else {
-
             jsonResponse({
-                file: path.join(__dirname, 'pdf.pdf'),
-                error: 'file was not created'
-            })
+                error: e.toString(),
+                status: sel.status
+            });
         }
-    }
-    catch (e) {
 
-        jsonResponse({
-            error: e.toString(),
-            status: sel.status
-        });
-    }
+        purl = false;
 
-    purl = false;
+
+    })
+    .catch(function (err) {
+        res.statusCode = 500
+        res.end(err.message)
+    });
 };
 
 const queue = function (handler) {
