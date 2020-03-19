9ae58f594a986d778bc8349cd3b9d063674b6a6c
stopsopa <grytysek@gmail.com>
2020-03-19 01:55:31 +0000

    up

--- stress-test.js
+++ stress-test.js
@@ -167,7 +167,7 @@ const run = async target => {
         if (data.state !== 'not-expired') {
 
             log.dump({
-                stress_return_error: data,
+                stress_test_file_generated: data,
             }, 20)
         }
     }
